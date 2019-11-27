efec2e5f88031cc0977d595b08806ae6c5ff90b0
Joaby Oliveira <joaby.santos@etec.sp.gov.br>
2019-11-27 11:48:50 -0300

    Doing arrays to show seats

--- C++/Teatro.cpp
+++ C++/Teatro.cpp
@@ -1,8 +1,33 @@
-#include<stdio.h>
-#include<windows.h>
-#include<locale.h>
+#include <stdio.h>
+#include <windows.h>
+#include <locale.h>
 
 int main(){
 	setlocale(LC_ALL, "portuguese");
-		
+	char chair[20][25];
+	int alt = 0, larg = 0;
+	
+    printf("");
+	scanf("%i", &alt);
+
+	printf("");
+	scanf("%i", &larg);
+
+    larg = larg - 1;
+	alt = alt - 1;
+
+	for(int i = 0; i <= alt; i++){
+		for(int j = 0; j <= larg; j++){
+			chair[i][j] = ' ' ;
+
+		}
+	}
+	for(int i = 0; i <= alt; i++){
+		for(int j = 0; j <= larg; j++){
+            printf("| |");
+			if(j == larg){
+				printf("\n");
+			}
+		}
+	}
 }
